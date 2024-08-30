3de8978c374c62041375b71dda71a58c7e8db45a
Daniel Rocha <161393763+DannKes1@users.noreply.github.com>
2024-08-30 19:23:58 -0400

    Crud do banco de dados SistemaFinanceiro
    Trabalho final de POO, nesse trabalho foi realizado um crud do banco de dados SistemaFinanceiro.

---
+++ CadastroFuncionarios/Program.cs
@@ -0,0 +1,284 @@
+﻿using CadastroFuncionarios.dao;
+using CadastroFuncionarios.Models;
+using System.Runtime.CompilerServices;
+
+namespace CadastroFuncionarios
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Conexao.Conectar();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao conectar:" + ex.Message);
+            }
+
+            bool chamarMenu = true;
+            while (chamarMenu)
+            {
+                chamarMenu = Menu();
+            }
+
+        }
+
+        private static bool Menu()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("CADASTRO DE FUNCIONÁRIOS");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("Escolha um opção: ");
+            Console.WriteLine("1) Digite 01 - Cadastrar funcionário no banco de dados. ");
+            Console.WriteLine("2) Digite 02 - Gerar lista de funcionários. ");
+            Console.WriteLine("3) Digite 03 - Deletar funcionário do banco de dados. ");
+            Console.WriteLine("4) Digite 04 - Alterar funcionário. ");
+            Console.WriteLine("5) Digite 05 - Sair da aplicação. ");
+
+            string resp = Console.ReadLine();
+
+            switch (resp)
+            {
+                case "01":
+                    Opcao1();
+                    return true;
+                    break;
+
+                case "02":
+                    Opcao02();
+                    return true;
+                    break;
+
+                case "03":
+                    Opcao03();
+                    return true;
+                    break;
+
+                case "04":
+                    Opcao04();
+                    return true;
+                    break;
+
+                case "05":
+                    Console.WriteLine("Você saiu da aplicação! ");
+                    return false;
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida, tente novamente. ");
+                    Console.ReadLine();
+                    return true;
+            }
+
+        }
+        private static void Opcao1()
+        {
+            /*insert*/
+            Console.Clear();
+            Funcionario funcionario01 = new Funcionario();
+            Console.WriteLine("Você escolheu a opção 01");
+            Console.WriteLine();
+            Console.WriteLine("Digite o nome do funcionario: ");
+            funcionario01.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o CPF: ");
+            funcionario01.Cpf = Console.ReadLine();
+
+
+            FuncionarioDAO f1 = new FuncionarioDAO();
+            f1.Insert(funcionario01);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Digite qualquer tecla para retornar ao menu...");
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+
+        private static void Opcao02()
+        {
+            Console.Clear();
+
+            FuncionarioDAO f1 = new FuncionarioDAO();
+            /* gerar lista*/
+            Console.WriteLine("Você escolheu a opção 02");
+            Console.WriteLine();
+
+            List<Funcionario> lista = f1.List();
+            foreach (var item in lista)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("ID_Funcionario: ");
+                Console.ResetColor();
+
+                Console.Write(item.IdFuncionario);
+
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("  Nome_Funcionario: ");
+                Console.ResetColor();
+                Console.Write(item.Nome);
+
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("  CPF_Funcionario: ");
+                Console.ResetColor();
+
+                Console.Write(item.Cpf);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Digite qualquer tecla para retornar ao menu...");
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+
+        private static void Opcao03()
+        {
+            Console.Clear();
+            try
+            {
+                FuncionarioDAO f1 = new FuncionarioDAO();
+                List<Funcionario> lista = f1.List();
+                /*delete*/
+                Console.WriteLine("Você escolheu a opção 03");
+                Console.WriteLine();
+                Console.WriteLine("Digite o ID que deseja deletar do banco de dados: ");
+                var idfunc = Convert.ToInt32(Console.ReadLine());
+
+                if (idfunc == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Nenhum funcionário encontrado com o ID fornecido.");
+                    Console.ReadKey();
+                    Console.ResetColor();
+
+                }
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Tem certeza que deseja deletar o funcionário com o ID: {idfunc}?");
+                Console.WriteLine("Digite (Sim) para confirmar");
+                Console.WriteLine("Digite (Não) para cancelar");
+
+                Console.ResetColor();
+                string resp = Console.ReadLine();
+                resp = resp.ToUpper();
+                switch (resp)
+                {
+                    case "SIM":
+                        f1.Delete(lista.Find(x => idfunc == x.IdFuncionario));
+                        break;
+                    case "NÃO":
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Opção cancelada com sucesso!");
+                        Console.ResetColor();
+                        Console.ReadKey();
+                        Opcao03();
+                        break;
+
+              
+
+                        default:
+                        Console.WriteLine();
+                        Console.ForegroundColor= ConsoleColor.DarkRed;
+                        Console.WriteLine("Você digitou algo errado, tente novamente");
+                        Console.ResetColor();
+                        Console.ReadKey();
+                        Opcao03();
+                        break;
+                }
+
+             
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Erro: " + ex.Message);
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+            Console.ForegroundColor= ConsoleColor.DarkRed;
+            Console.WriteLine("Digite qualquer tecla para retornar ao menu...");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
+        private static void Opcao04()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Você escolheu a opção 04");
+                Console.WriteLine();
+                FuncionarioDAO f1 = new FuncionarioDAO();
+                List<Funcionario> lista = f1.List();
+
+                Console.WriteLine("Digite o ID do funcionário no qual deseja fazer o Update: ");
+                var idfunci = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                var verificacao = lista.Find(x => idfunci == x.IdFuncionario);
+
+                if (verificacao == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Nenhum funcionário encontrado com o ID fornecido.");
+                    Console.ReadKey();
+                    Console.ResetColor();
+                    return;
+                }
+
+                Console.WriteLine("O que deseja alterar? (Escolha uma opção)");
+                Console.WriteLine("1) Digite 01 - Alterar o Nome e CPF do funcionário");
+                Console.WriteLine("2) Digite 02 - Alterar somente o Nome do funcionário");
+                Console.WriteLine("3) Digite 03 - Alterar somente o CPF do funcionário");
+                string resp = Console.ReadLine();
+                Console.WriteLine();
+                switch (resp)
+                {
+                    case "01":
+                        Console.WriteLine("Digite o novo nome do funcionário: ");
+                        verificacao.Nome = Console.ReadLine();
+                        Console.WriteLine("Digite o novo CPF do funcionário: ");
+                        verificacao.Cpf = Console.ReadLine();
+                        break;
+
+                    case "02":
+                        Console.WriteLine("Digite o novo nome do funcionário: ");
+                        verificacao.Nome = Console.ReadLine();
+                        break;
+
+                    case "03":
+                        Console.WriteLine("Digite o novo CPF do funcionário: ");
+                        verificacao.Cpf = Console.ReadLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida, tente novamente. ");
+                        Console.ReadLine();
+                        break; 
+                }
+                f1.Update(verificacao);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+            Console.WriteLine();
+            Console.ForegroundColor= ConsoleColor.DarkRed;
+            Console.WriteLine("Digite qualquer tecla para retornar ao menu...");
+            Console.ResetColor();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ CadastroFuncionarios/dao/FuncionarioDAO.cs
@@ -0,0 +1,147 @@
+﻿using CadastroFuncionarios.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroFuncionarios.dao
+{
+    internal class FuncionarioDAO
+    {
+        public void Insert(Funcionario funcionario)
+        {
+            if (string.IsNullOrWhiteSpace(funcionario.Nome))
+            {
+                throw new Exception("O nome do funcionário não pode ser vazio ou nulo.");
+            }
+
+            if (string.IsNullOrWhiteSpace(funcionario.Cpf))
+            {
+                throw new Exception("O CPF do funcionário não pode ser vazio ou nulo.");
+            }
+
+            try
+            {
+
+                string sql = "INSERT INTO Funcionario (Nome_Funcionario, CPF_Funcionario) " +
+                    "VALUES(@nome, @cpf)";
+                MySqlCommand comando = new MySqlCommand(sql, Conexao.Conectar());
+                comando.Parameters.AddWithValue("@nome", funcionario.Nome);
+                comando.Parameters.AddWithValue("@cpf", funcionario.Cpf);
+                comando.ExecuteNonQuery();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Funcionario cadastrado com Sucesso!");
+                Console.ResetColor();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao cadastrar o funcionario!{ex.Message}");
+
+            }
+
+            finally
+            {
+                Conexao.FecharConexao();
+            }
+        }
+        public void Delete(Funcionario funcionario)
+        {
+
+            if (funcionario == null)
+            {
+                throw new Exception("Funcionário não existe na lista ou o valor inserido é nulo. Por gentileza, tente novamente!");
+            }
+
+            try
+            {
+                
+                string sql = "DELETE FROM Funcionario WHERE Id_Funcionario = @IdFuncionario";
+                MySqlCommand comando = new MySqlCommand(sql, Conexao.Conectar());
+                comando.Parameters.AddWithValue("@IdFuncionario", funcionario.IdFuncionario);
+
+                comando.ExecuteNonQuery();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Cliente excluido com sucesso!");
+                Console.ReadKey();
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao excluir o cliente {ex.Message}");
+            }
+
+            finally
+            {
+                Conexao.FecharConexao();
+            }
+        }
+
+        public List<Funcionario> List()
+        {
+            List<Funcionario> funcionarios = new List<Funcionario>();
+            try
+            {
+                var sql = "SELECT * FROM Funcionario ORDER BY Nome_Funcionario";
+                MySqlCommand comando = new MySqlCommand(sql, Conexao.Conectar());
+                using (MySqlDataReader dr = comando.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Funcionario funcionario = new Funcionario();
+                        funcionario.IdFuncionario = dr.GetInt32("Id_Funcionario");
+                        funcionario.Nome = dr.GetString("Nome_Funcionario");
+                        funcionario.Cpf = dr.GetString("CPF_Funcionario");
+                        funcionarios.Add(funcionario);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao cadastrar na lista{ex.Message}");
+            }
+            finally
+            {
+                Conexao.FecharConexao();
+
+            }
+            return funcionarios;
+        }
+
+        public void Update(Funcionario funcionario)
+        {
+            if(funcionario == null)
+            {
+                throw new Exception("Funcionário não pode ser nulo. ");
+            }
+            try
+            {
+                string sql = "UPDATE Funcionario SET Nome_Funcionario = @nome, CPF_Funcionario = @cpf " +
+                   "WHERE Id_Funcionario = @IdFuncionario";
+                MySqlCommand comando = new MySqlCommand(sql, Conexao.Conectar());
+                comando.Parameters.AddWithValue("@nome", funcionario.Nome);
+                comando.Parameters.AddWithValue("@cpf", funcionario.Cpf);
+                comando.Parameters.AddWithValue("@IdFuncionario", funcionario.IdFuncionario);
+                comando.ExecuteNonQuery();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Cliente atualizado com sucesso! ");
+                Console.ReadKey();
+                Console.ResetColor();
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception($"Erro ao atualizar o cliente {ex.Message}");
+            }
+            finally
+            {
+                Conexao.FecharConexao();
+            }
+        } 
+        
+
+    }
+}
